35c79a7540c14426060e246486ea3fd18c08786c
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-03-25 16:31:18 +0000

    added home app and templates

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """ A view that returns the index page """
+
+    return render(request, 'home/index.html')
