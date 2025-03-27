650f82ea16b47916dc5b7f4bcedd77ffb8ea6a9b
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-03-27 16:51:19 +0000

    update products.json and categories.json. update file paths

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', #required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
