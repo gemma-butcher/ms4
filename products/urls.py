9c8522c04087a54993b8ee8907af77e432d2ea1d
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-03-28 14:19:04 +0000

    added product details functionality

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    # path('product_id', views.product_detail, name='product_detail'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),  # Fixed this line
 ]
