faa8f8533ad8bdd2710d5a21c4837471eadc06f2
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-03-28 17:03:26 +0000

    added category search urls

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 # Create your views here.
 
 def all_products(request):
@@ -9,20 +9,30 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
-    if request.GET:
-        if 'q' in request.GET:
-            query = request.GET.get('q')
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
+
+    if 'category' in request.GET:
+        categories = request.GET['category'].split(',')
+        products = products.filter(category__name__in=categories)
+        categories = Category.objects.filter(name__in=categories)
+    else:
+        categories = Category.objects.all()
+
+    if 'q' in request.GET:
+        query = request.GET.get('q').strip
+
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse('products'))
+        
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
     
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
     return render(request, 'products/products.html', context)
 
@@ -31,7 +41,6 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
-    # print("🛠 DEBUG: View is being accessed. Found", len(products), "products.")  # Debug line
     
     context = {
         'product': product,
