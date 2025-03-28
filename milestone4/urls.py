1351b3149cfe92c6e51879bfd5ddcf9036a65061
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-03-28 10:24:00 +0000

    add products template and settings

--- milestone4/urls.py
+++ milestone4/urls.py
@@ -23,7 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
