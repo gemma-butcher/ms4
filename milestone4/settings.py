d7dcfa4a8bbda77e4810f4c380c3154d5f1a4c3b
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-04-07 16:24:29 +0100

    add toast message templates. Add message storage

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -81,6 +81,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'milestone4.wsgi.application'
 
+# Messages
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
