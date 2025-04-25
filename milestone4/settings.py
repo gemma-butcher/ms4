a505d579b33941564be5361689f0eb5113fab035
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-04-25 12:05:45 +0100

    add webhook functionality and json print testing

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = 'django-insecure-m8ma#ckyf-tmy(c(9bduh2(3_s6779yuo*o#&le!xlw3ps4mv-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
