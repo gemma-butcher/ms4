7aa13ae5ee2abc08f833493f78e03334b8dec6b0
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-05-02 12:54:19 +0100

    add contact app and page

---
+++ contact/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Contact
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        # Save to database
+        Contact.objects.create(name=name, email=email)
+        return HttpResponse("Thank you for contacting us!")
+    return render(request, 'contact/contact.html')
