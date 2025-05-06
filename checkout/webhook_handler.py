7c523cf4cbc6c729ac9e45389a149494230abc3a
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-05-06 17:27:40 +0100

    add confirmation email and add image folder for documentation

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
 from .models import Order, OrderLineItem
 from products.models import Product
 from profiles.models import UserProfile
@@ -11,6 +15,27 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """
+        Send the user a confirmation email when the order is created
+        """
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        print("Sending confirmation email...")
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
+        print("Email sent!")
 
     def handle_event(self, event):
         """
@@ -88,11 +113,15 @@ class StripeWH_Handler:
                 time.sleep(1)
 
         if order_exists:
+            print("Order exists. Sending confirmation email...")
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
 
-        # If order doesn't exist, create a new one
+        else:
+            print("Order does not exist. Creating a new order...")
+            order = None
         try:
             order = Order.objects.create(
                 full_name=shipping_details.name,
@@ -128,14 +157,14 @@ class StripeWH_Handler:
                             product_size=size,
                         )
                         order_line_item.save()
-
+        
         except Exception as e:
             if order:
                 order.delete()
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | ERROR: {e}',
                 status=500)
-
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
