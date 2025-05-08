f92b0af5dc7f04897b0ff4f5c145e4079de5ac6d
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-05-08 16:59:38 +0100

    add product form, view and url added. Template created

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     # path('product_id', views.product_detail, name='product_detail'),
-    path('product/<int:product_id>/', views.product_detail, name='product_detail'),  # Fixed this line
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
