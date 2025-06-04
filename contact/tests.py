28d2ec44f51fa4125242362d45da238da7f44b6d
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-06-04 15:59:53 +0100

    remove old files and tidy code

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ContactViewTest(TestCase):
+    def test_contact_get(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
