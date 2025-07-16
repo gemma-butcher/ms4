aa5c87034fe89f2107158069c5b22564c86ce558
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-07-16 15:19:45 +0100

    update settings

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -21,7 +21,6 @@ if os.path.exists("env.py"):
     import env
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -58,7 +57,7 @@ INSTALLED_APPS = [
     'faq',
     'contact',
     'profiles',
-    
+    'newsletter.apps.NewsletterConfig',
 
     # Other
     'crispy_forms',
@@ -92,8 +91,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),  # Project-level templates
-            os.path.join(BASE_DIR, 'templates', 'allauth', 'templates'),  # Allauth templates
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth', 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -140,16 +139,28 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
 
@@ -230,7 +241,6 @@ if 'USE_AWS' in os.environ and os.environ['USE_AWS'] == 'True':
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-
 
 
 # Default primary key field type
