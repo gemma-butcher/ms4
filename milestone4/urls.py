a2bbb39ba04da12f7e0c55e64a33f525f7f52a94
Gemma Butcher <159952112+GemmaButcher-dev@users.noreply.github.com>
2025-07-17 10:11:39 +0100

    add services page and fix responsiivity

--- milestone4/urls.py
+++ milestone4/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +15,11 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('profiles/', include('profiles.urls')),
     path('newsletter/', include('newsletter.urls')),
+    path(
+        'services/',
+        TemplateView.as_view(template_name="services.html"),
+        name='services'
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Enable media file serving in development only
